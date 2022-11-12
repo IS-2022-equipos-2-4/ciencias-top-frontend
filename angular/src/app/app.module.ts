e357c3cc21715005b5da6709b4f34225c52bf610
SirMaik <mianorsi@ciencias.unam.mx>
2022-11-11 19:49:13 -0600

    Usuarios

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductosComponent } from './productos/productos.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/productos', pathMatch: 'full' },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'productos', component: ProductosComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ProductosComponent,
+    UsuariosComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

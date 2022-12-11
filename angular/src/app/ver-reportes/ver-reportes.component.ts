57ea889bd737411d4a230fc6b3e4e0f949d799e8
JoseEGJasso <josejasso2000@ciencias.unam.mx>
2022-12-11 17:47:25 -0600

    update: agregocasos iii, iv y v

--- angular/src/app/ver-reportes/ver-reportes.component.ts
+++ angular/src/app/ver-reportes/ver-reportes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Producto } from '../productos/producto';
 import { ProductosMes } from './productosMes';
+import { UsuarioDevolTardiasDTO } from './usuariosDevolTardias.dto';
 import { UsuariosPorCarrera } from './usuariosPorCarrera';
 import { UsuariosRentasSemana } from './usuariosRentasSemana';
 import { VerReportesService } from './ver-reportes.service';
@@ -14,6 +16,10 @@ export class VerReportesComponent implements OnInit {
   productosMes: ProductosMes[];
   usuariosPorCarrera: UsuariosPorCarrera[];
   usuariosRentasSemana: UsuariosRentasSemana[];
+
+  usuariosDevolTardias: UsuarioDevolTardiasDTO[];
+  productosMenorCosto: Producto[];
+  numCuentasInactivas: number;
   
   constructor(
     private router: Router,
@@ -37,6 +43,24 @@ export class VerReportesComponent implements OnInit {
         .getUsuariosRentasSemanal()
         .subscribe((usuariosRentasSemana) => {
           this.usuariosRentasSemana = usuariosRentasSemana;
+        })
+
+      this.verReportesService
+        .getUsuariosConMasDevolucionesTardias()
+        .subscribe((usuariosDevolTardias) => {
+          this.usuariosDevolTardias = usuariosDevolTardias;
+        })
+
+      this.verReportesService
+        .getProductosMenorCosto()
+        .subscribe((productosMenorCosto) => {
+          this.productosMenorCosto = productosMenorCosto;
+        })
+
+      this.verReportesService
+        .getNumInactivos()
+        .subscribe((numCuentasInactivas) => {
+          this.numCuentasInactivas = numCuentasInactivas;
         })
     }
 }
